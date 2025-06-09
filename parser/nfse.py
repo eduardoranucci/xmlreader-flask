36fdc00e83968830da5171fa2cb44d6864402bbd
eduardoranucci <eduardoranucci@outlook.com>
2025-06-08 22:33:27 -0300

    feat: reorder columns in nfse .xlsx table

--- parser/nfse.py
+++ parser/nfse.py
@@ -21,11 +21,27 @@ def parser_nfse(xml_data):
         # Dados gerais
         data["Número"] = get_text(nfse, './/Numero', ns)
         data["Data Emissão"] = parse_date(get_text(nfse, './/DataEmissao', ns))
+
+        # Tomador de Serviço
+        taker = get_text(tomador, './/IdentificacaoTomador/CpfCnpj/Cnpj', ns)
+        if taker is None:
+            taker = get_text(tomador, './/IdentificacaoTomador/CpfCnpj/Cpf', ns)
+        data["CNPJ/CPF Tomador"] = taker
+        data["Razão Social Tomador"] = get_text(tomador, './/RazaoSocial', ns)
+        data["Código Município Tomador"] = get_text(tomador, './/Endereco/CodigoMunicipio', ns)
+        data["UF Tomador"] = get_text(tomador, './/Endereco/Uf', ns)
+        
+        # Prestador de Serviço
+        data["CNPJ Prestador"] = get_text(prestador, './/IdentificacaoPrestador/Cnpj', ns)
+        data["Inscrição Municipal Prestador"] = get_text(prestador, './/IdentificacaoPrestador/InscricaoMunicipal', ns)
+        data["Nome Fantasia Prestador"] = get_text(prestador, './/NomeFantasia', ns)
+        data["Código Município Prestador"] = get_text(prestador, './/Endereco/CodigoMunicipio', ns)
+        data["UF Prestador"] = get_text(prestador, './/Endereco/Uf', ns)
+
         data["Natureza"] = get_text(nfse, './/NaturezaOperacao', ns)
         data["Regime Especial"] = get_text(nfse, './/RegimeEspecialTributacao', ns)
         data["Optante Simples Nacional"] = get_text(nfse, './/OptanteSimplesNacional', ns)
         data["Incentivador Cultural"] = get_text(nfse, './/IncentivadorCultural', ns)
-        data["Valor Crédito"] = get_text(nfse, './/ValorCredito', ns)
 
         # RPS
         data["Número RPS"] = get_text(rps, './/Numero', ns)
@@ -35,38 +51,26 @@ def parser_nfse(xml_data):
 
         # Serviço
         data["Valor Serviços"] = get_text(servico, './/Valores/ValorServicos', ns)
+        data["Valor Crédito"] = get_text(nfse, './/ValorCredito', ns)
         data["Valor Deduções"] = get_text(servico, './/Valores/ValorDeducoes', ns)
-        data["Valor Pis"] = get_text(servico, './/Valores/ValorPis', ns)
-        data["Valor Cofins"] = get_text(servico, './/Valores/ValorCofins', ns)
-        data["Valor Inss"] = get_text(servico, './/Valores/ValorInss', ns)
-        data["Valor Ir"] = get_text(servico, './/Valores/ValorIr', ns)
-        data["Valor Csll"] = get_text(servico, './/Valores/ValorCsll', ns)
+        data["Valor Líquido NFSe"] = get_text(servico, './/Valores/ValorLiquidoNfse', ns)
+        data["Pis"] = get_text(servico, './/Valores/ValorPis', ns)
+        data["Cofins"] = get_text(servico, './/Valores/ValorCofins', ns)
+        data["Inss"] = get_text(servico, './/Valores/ValorInss', ns)
+        data["Ir"] = get_text(servico, './/Valores/ValorIr', ns)
+        data["Csll"] = get_text(servico, './/Valores/ValorCsll', ns)
         data["ISS Retido"] = get_text(servico, './/Valores/IssRetido', ns)
-        data["Valor ISS"] = get_text(servico, './/Valores/ValorIss', ns)
-        data["Valor ISS Retido"] = get_text(servico, './/Valores/ValorIssRetido', ns)
+        data["ISS"] = get_text(servico, './/Valores/ValorIss', ns)
+        data["ISS Retido"] = get_text(servico, './/Valores/ValorIssRetido', ns)
         data["Outras Retenções"] = get_text(servico, './/Valores/OutrasRetencoes', ns)
         data["Base Cálculo"] = get_text(servico, './/Valores/BaseCalculo', ns)
         data["Alíquota"] = get_text(servico, './/Valores/Aliquota', ns)
         data["Desconto Incondicionado"] = get_text(servico, './/Valores/DescontoIncondicionado', ns)
         data["Desconto Condicionado"] = get_text(servico, './/Valores/DescontoCondicionado', ns)
-        data["Valor Líquido NFSe"] = get_text(servico, './/Valores/ValorLiquidoNfse', ns)
         
         data["Item Lista Serviço"] = get_text(nfse, './/ItemListaServico', ns)
         data["Código CNAE"] = get_text(nfse, './/CodigoCnae', ns)
         data["Código Município"] = get_text(nfse, './/CodigoMunicipio', ns)
-
-        # Prestador de Serviço
-        data["CNPJ Prestador"] = get_text(prestador, './/IdentificacaoPrestador/Cnpj', ns)
-        data["Inscrição Municipal Prestador"] = get_text(prestador, './/IdentificacaoPrestador/InscricaoMunicipal', ns)
-        data["Nome Fantasia Prestador"] = get_text(prestador, './/NomeFantasia', ns)
-        data["Código Município Prestador"] = get_text(prestador, './/Endereco/CodigoMunicipio', ns)
-        data["UF Prestador"] = get_text(prestador, './/Endereco/Uf', ns)
-
-        # Tomador de Serviço
-        data["CNPJ Tomador"] = get_text(tomador, './/IdentificacaoTomador/CpfCnpj/Cnpj', ns)
-        data["Razão Social Tomador"] = get_text(tomador, './/RazaoSocial', ns)
-        data["Código Município Tomador"] = get_text(tomador, './/Endereco/CodigoMunicipio', ns)
-        data["UF Tomador"] = get_text(tomador, './/Endereco/Uf', ns)
 
         nfses.append(data)
 
